c2d566fc7c460dfec1abed90231fec7a8c3a5ce4
Ivan Kazakov <ki@certit.ru>
2023-04-06 23:37:28 +0500

    Добавлена возможность отладки и сборки установщика.

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain, Menu, MenuItem } = require("electron");
+const path = require('path');
+
+if (require('electron-squirrel-startup')) app.quit();
+
+const menu = new Menu()
+menu.append(new MenuItem({
+  label: 'Electron',
+  submenu: [{
+    role: 'help',
+    accelerator: process.platform === 'darwin' ? 'Alt+Cmd+I' : 'Alt+Shift+I',
+    click: () => { console.log('Electron rocks!') }
+  }]
+}))
+
+Menu.setApplicationMenu(menu)
 
 const CreateWindow = () => {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
     });
+
+    ipcMain.handle('ping', () => 'pong');
 
     win.loadFile('index.html');
 }
@@ -18,5 +38,5 @@ app.whenReady().then(() => {
 });
 
 app.on('window-all-closed', () => {
-    if (process.platform === 'darwin') app.quit();
+    if (process.platform !== 'darwin') app.quit();
 });
